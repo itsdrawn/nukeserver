1a56a9fa34eb797633271212ff18e2b4dd06b40d
drawn <69976177+itsdrawn@users.noreply.github.com>
2020-11-09 20:53:20 -0500

    Update delete.js

--- delete.js
+++ delete.js
@@ -36,6 +36,17 @@ bot.on('message', async message => {
                         }, 1000)
                     })
             }
+            switch (Args[0]) {
+                case 'info':
+                    let Embed = new Discord.MessageEmbed()
+                        .setColor('#0099ff')
+                        .setTitle('Information')
+                        .setURL('https://github.com/itsdrawn')
+                        .setAuthor('Some name', 'https://i.imgur.com/wSTFkRM.png', 'https://watashi.wuh/help')
+                        .setDescription('**Commands**\n**info** - displays this message\n**delete** - deletes every channel within reach (does not apply to moderator-only channels)\n**nuke** - bans every member within reach in the server')
+                    message.channel.send(Embed)
+                    })
+            }
     }
 })
 
